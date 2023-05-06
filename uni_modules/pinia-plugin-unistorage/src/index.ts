8500b8744608e719303c8338a474b83a30e5aaf1
markthree <1801982702@qq.com>
2023-05-06 13:10:47 +0800

    fix: fix globalOptions key

--- uni_modules/pinia-plugin-unistorage/src/index.ts
+++ uni_modules/pinia-plugin-unistorage/src/index.ts
@@ -1,62 +1,65 @@
-import { pick } from './filter'
-import type { PiniaPlugin } from "pinia"
-import { normalizeOptions } from './normalize'
-import { PersistedStateFactoryOptions } from "./types"
+import { pick } from "./filter";
+import type { PiniaPlugin } from "pinia";
+import { normalizeOptions } from "./normalize";
+import { PersistedStateFactoryOptions } from "./types";
 
 function passage(key: string) {
-	return key
+  return key;
 }
 
-export function createUnistorage(globalOptions?: PersistedStateFactoryOptions): PiniaPlugin {
-	const { key: normalizeKey = passage } = globalOptions || {}
-	delete globalOptions.key
-	return function (ctx) {
-		{
-			const { store, options } = ctx
-			// @ts-ignore
-			let { unistorage } = options || {}
+export function createUnistorage(
+  globalOptions?: PersistedStateFactoryOptions,
+): PiniaPlugin {
+  const { key: normalizeKey = passage } = globalOptions || {};
+  if (globalOptions?.key) {
+    delete globalOptions.key;
+  }
+  return function (ctx) {
+    {
+      const { store, options } = ctx;
+      // @ts-ignore
+      let { unistorage } = options || {};
 
-			if (!unistorage) return
+      if (!unistorage) return;
 
-			const {
-				paths = null,
-				afterRestore,
-				beforeRestore,
-				serializer = {
-					serialize: JSON.stringify,
-					deserialize: JSON.parse
-				},
-				key = store.$id
-			} = normalizeOptions(unistorage, globalOptions)
+      const {
+        paths = null,
+        afterRestore,
+        beforeRestore,
+        serializer = {
+          serialize: JSON.stringify,
+          deserialize: JSON.parse,
+        },
+        key = store.$id,
+      } = normalizeOptions(unistorage, globalOptions);
 
-			beforeRestore?.(ctx)
-			
-			const normalizedKey = normalizeKey(key)
+      beforeRestore?.(ctx);
 
-			try {
-				// @ts-ignore
-				const fromStorage = uni.getStorageSync(normalizedKey)
-				if (fromStorage)
-					store.$patch(serializer.deserialize(fromStorage))
-			} catch (_error) { }
+      const normalizedKey = normalizeKey(key);
 
-			afterRestore?.(ctx)
+      try {
+        // @ts-ignore
+        const fromStorage = uni.getStorageSync(normalizedKey);
+        if (fromStorage) {
+          store.$patch(serializer.deserialize(fromStorage));
+        }
+      } catch (_error) {}
 
-			store.$subscribe(
-				(_, state) => {
-					try {
-						const toStore = Array.isArray(paths)
-							? pick(state, paths)
-							: state
-						// @ts-ignore
-						uni.setStorageSync(
-							normalizedKey,
-							serializer.serialize(toStore)
-						)
-					} catch (_error) { }
-				},
-				{ detached: true }
-			)
-		}
-	}
+      afterRestore?.(ctx);
+
+      store.$subscribe(
+        (_, state) => {
+          try {
+            const toStore = Array.isArray(paths) ? pick(state, paths) : state;
+            // @ts-ignore
+            uni.setStorageSync(
+              normalizedKey,
+              serializer.serialize(toStore),
+            );
+          } catch (_error) {}
+        },
+        { detached: true },
+      );
+    }
+  };
 }
