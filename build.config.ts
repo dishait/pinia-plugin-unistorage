0a6272909a712bc054a3ff02c876eaf6e80ca0d7
markthree <1801982702@qq.com>
2024-07-10 18:46:42 +0800

    chore!: 继承 pinia-plugin-persistedstate

--- build.config.ts
+++ build.config.ts
@@ -1,12 +1,12 @@
-import { copyFile } from "fs/promises";
+import { copyFile, readFile, writeFile } from "fs/promises";
 import { defineBuildConfig } from "unbuild";
 
 export default defineBuildConfig({
   failOnWarn: false,
-  declaration: true,
+  declaration: false,
   clean: false,
   rollup: {
-    emitCJS: true,
+    emitCJS: false,
   },
   entries: [{
     builder: "rollup",
@@ -14,10 +14,34 @@ export default defineBuildConfig({
     input: "./uni_modules/pinia-plugin-unistorage/src",
   }],
   hooks: {
-    "build:done"(ctx) {
+    async "build:done"(ctx) {
       copyFile(
         "dist/index.mjs",
         "./uni_modules/pinia-plugin-unistorage/index.js",
+      );
+
+      const dts = await readFile(
+        "node_modules/pinia-plugin-persistedstate/dist/index.d.mts",
+        "utf-8",
+      );
+
+      const newDts = dts + `
+/**
+ * Creates a pinia persistence plugin with uniapp
+ * @param factoryOptions global persistence options
+ * @returns pinia plugin
+ */
+declare function createUnistorage(factoryOptions?: PersistedStateFactoryOptions): PiniaPlugin;
+      `;
+
+      await writeFile(
+        "./uni_modules/pinia-plugin-unistorage/index.d.ts",
+        newDts,
+      );
+
+      await writeFile(
+        "./dist/index.d.ts",
+        newDts,
       );
     },
   },
--- uni_modules/pinia-plugin-unistorage/src/index.ts
+++ uni_modules/pinia-plugin-unistorage/src/index.ts
@@ -1,65 +1,35 @@
-import { pick } from "./filter";
-import type { PiniaPlugin } from "pinia";
-import { normalizeOptions } from "./normalize";
-import { PersistedStateFactoryOptions } from "./types";
+import {
+  createPersistedState,
+  type PersistedStateFactoryOptions,
+} from "pinia-plugin-persistedstate";
 
-function passage(key: string) {
-  return key;
-}
+export * from "pinia-plugin-persistedstate";
 
 export function createUnistorage(
   globalOptions: PersistedStateFactoryOptions = {},
-): PiniaPlugin {
-  const { key: normalizeKey = passage } = globalOptions || {};
-  if (globalOptions?.key) {
-    delete globalOptions.key;
-  }
-  return function (ctx) {
-    {
-      const { store, options } = ctx;
-      // @ts-ignore
-      let { unistorage } = options || {};
-
-      if (!unistorage) return;
-
-      const {
-        paths = null,
-        afterRestore,
-        beforeRestore,
-        serializer = {
-          serialize: JSON.stringify,
-          deserialize: JSON.parse,
-        },
-        key = store.$id,
-      } = normalizeOptions(unistorage, globalOptions);
-
-      beforeRestore?.(ctx);
-
-      const normalizedKey = normalizeKey(key);
-
-      try {
+) {
+  const persistedState = createPersistedState({
+    storage: {
+      getItem(key) {
         // @ts-ignore
-        const fromStorage = uni.getStorageSync(normalizedKey);
-        if (fromStorage) {
-          store.$patch(serializer.deserialize(fromStorage));
-        }
-      } catch (_error) {}
-
-      afterRestore?.(ctx);
-
-      store.$subscribe(
-        (_, state) => {
-          try {
-            const toStore = Array.isArray(paths) ? pick(state, paths) : state;
-            // @ts-ignore
-            uni.setStorageSync(
-              normalizedKey,
-              serializer.serialize(toStore),
-            );
-          } catch (_error) {}
-        },
-        { detached: true },
-      );
+        return uni.getStorageSync(key);
+      },
+      setItem(key, value) {
+        // @ts-ignore
+        uni.setStorageSync(key, value);
+      },
+    },
+    serializer: {
+      deserialize: JSON.parse,
+      serialize: JSON.stringify,
+    },
+    ...globalOptions,
+  });
+  // @ts-ignore
+  return (ctx) => {
+    if (ctx.options.unistorage) {
+      ctx.options.persist = ctx.options.unistorage;
     }
+    return persistedState(ctx);
   };
 }
